33f255811826bb342adbf14f78472a09903152a4
KAN <jianyu.yao88@gmail.com>
2024-04-16 21:23:57 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Meals from './components/Meals'
+import { useGlobalContext } from './context'
+import Modal from './components/Modal'
+import Favorite from './components/Favorite'
+import Search from './components/Search'
+
+function App() {
+  const { showModal, favoriteFood } = useGlobalContext()
+
+  return (
+    <div className='app-container'>
+      <Search />
+      {favoriteFood.length > 0 && <Favorite />}
+      <Meals />
+      { showModal && <Modal />}
+    </div>
+  )
+}
+
+export default App
